19bd21601621532288f8d2aa7a61b29e6542d8eb
Tiago <39215546+tiagovasc@users.noreply.github.com>
2023-08-13 17:10:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-document.getElementById("downArrow").addEventListener("click", function() {
-    var firstCaloriesValue = document.getElementById("calories0").value;
-    for (var i = 1; i < 10; i++) {
-        document.getElementById("calories" + i).value = firstCaloriesValue;
-    }
-});
-
-document.getElementById("wipe").addEventListener("click", function() {
-    for (var i = 0; i < 10; i++) {
-        document.getElementById("calories" + i).value = "";
-    }
-});
-
 document.getElementById("calculate").addEventListener("click", calculateMaintenance);
 
+document.getElementById("down-arrow").addEventListener("click", fillAllCalories);
+
+document.getElementById("cross").addEventListener("click", clearCalories);
+
 function calculateMaintenance() {
-    var totalCalories = 0;
-    var totalWeightLost = 0;
+  var totalCalories = 0;
+  var totalWeightLost = 0;
+  var totalCount = 0;
 
-    for (var i = 0; i < 9; i++) {
-        var weight = parseFloat(document.getElementById('weight' + i).value);
-        var nextWeight = parseFloat(document.getElementById('weight' + (i + 1)).value);
-        var calories = parseFloat(document.getElementById('calories' + i).value);
+  for (var i = 0; i < 9; i++) {
+    var weight = parseFloat(document.getElementById('weight' + i).value);
+    var nextWeight = parseFloat(document.getElementById('weight' + (i + 1)).value);
+    var calories = parseFloat(document.getElementById('calories' + i).value);
 
-        if (isNaN(weight) || isNaN(nextWeight) || isNaN(calories)) continue;
+    if (isNaN(weight) || isNaN(nextWeight) || isNaN(calories)) continue;
 
-        var weightLost = weight - nextWeight;
-        if (document.getElementById('unit').value === 'kg') {
-            weightLost *= 2.20462; // Convert kilograms to pounds
-        }
+    var weightLost = weight - nextWeight;
 
-        totalWeightLost += weightLost;
-        totalCalories += calories;
-    }
+    totalWeightLost += weightLost;
+    totalCalories += calories;
+    totalCount += 1;
+  }
 
-    var maintenanceCalories = totalCalories / (totalWeightLost > 0 ? totalWeightLost : 1) + 500;
-    document.getElementById('result').textContent = 'Maintenance Calories: ' + maintenanceCalories.toFixed(2);
+  var caloriesPerWeightUnit = document.getElementById('unit').value === 'kg' ? 7700 : 3500;
+  var totalDeficit = totalWeightLost * caloriesPerWeightUnit / 7; // Divide by 7 to get daily deficit
+  var maintenanceCalories = (totalCalories + totalDeficit) / (totalCount > 0 ? totalCount : 1);
+
+  document.getElementById('result').textContent = 'Maintenance Calories: ' + maintenanceCalories.toFixed(2);
+}
+
+function fillAllCalories() {
+  var firstCalorieValue = document.getElementById('calories0').value;
+  for (var i = 1; i < 10; i++) {
+    document.getElementById('calories' + i).value = firstCalorieValue;
+  }
+}
+
+function clearCalories() {
+  for (var i = 0; i < 10; i++) {
+    document.getElementById('calories' + i).value = "";
+  }
 }
